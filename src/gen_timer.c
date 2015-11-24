4b04da8e9f42f52158a988175fcd6e305d6c1de1
Peter Andersson <pelleplutt1976@gmail.com>
2015-11-24 09:10:39 +0100

    Fixed small gen_timer.c bug

--- src/gen_timer.c
+++ src/gen_timer.c
@@ -22,7 +22,7 @@ static inline void timer_decide_next_period(gen_tim *tim) {
 
 static inline void timer_await_splice(gen_tim *tim) {
 #if TIMER_LEAST_VALUE_BEFORE_OP > 0
-  while (TIMER_CALC_CURRENT_TICK(timer_hal_get_current(tim)) < TIMER_LEAST_VALUE_BEFORE_OP) {
+  while (TIMER_CALC_CURRENT_TICK(timer_hal_get_current(tim)) >= TIMER_MAX_TICKS - TIMER_LEAST_VALUE_BEFORE_OP) {
     // something is very soon to happen, do not meddle with state now
     TIMER_AWAIT_SPLICE_INSTR;
   }
